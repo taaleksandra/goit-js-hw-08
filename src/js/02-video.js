4b5326a092b27b91598472cddb2f88840505d6ec
taaleksandra <olatarnas@gmail.com>
2022-11-17 22:19:18 +0100

    2nd task in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+'use strict';
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// tutaj funkcja zapisująca czas odtswarzania
+const currentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data.seconds);
+}, 1000);
+
+player.on('timeupdate', currentTime);
