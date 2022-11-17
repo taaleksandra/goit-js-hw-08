6dcfea428c98622fb3273196053a763e6ba8949c
taaleksandra <olatarnas@gmail.com>
2022-11-17 22:34:51 +0100

    2nd task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,28 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-// tutaj funkcja zapisująca czas odtswarzania
+//saving current time to local storage
 const currentTime = throttle(data => {
   localStorage.setItem('videoplayer-current-time', data.seconds);
-  console.log(data.seconds);
 }, 1000);
 
 player.on('timeupdate', currentTime);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
