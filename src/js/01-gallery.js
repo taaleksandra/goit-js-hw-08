3d6d9ce17f40ed0e47c0f2b94abb48a1efbf5dd9
taaleksandra <olatarnas@gmail.com>
2022-11-17 13:41:33 +0100

    1st task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+// making gallery elements
+galleryItems.forEach(item => {
+  const imageLink = document.createElement('a');
+  imageLink.classList.add('gallery__item');
+  imageLink.setAttribute('href', item.original);
+
+  const imagePhoto = document.createElement('img');
+  imagePhoto.classList.add('gallery__image');
+  imagePhoto.src = item.preview;
+  imagePhoto.setAttribute('alt', item.description);
+
+  imageLink.append(imagePhoto);
+  gallery.append(imageLink);
+});
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
